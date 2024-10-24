8619089ff6420eaa80b66a958157b38958aa654d
DaniloYP <daniloyp@gmail.com>
2024-10-24 23:09:22 +0000

    cartas aleatorias danilo ok

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let card1 = document.querySelector(".card");
+let caja = document.querySelector(".container");
+let numeros = document.getElementById("number");
+let iconosTop = document.getElementById("top");
+let iconosBotton = document.getElementById("botton");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let iconos = ["♦", "♥", "♠", "♣"];
+let valores = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function seleccionarCard() {
+  let randomIcons = Math.floor(Math.random() * iconos.length);
+  let randomValue = Math.floor(Math.random() * valores.length);
+  numeros.innerHTML = valores[randomValue];
+  iconosTop.innerHTML = iconos[randomIcons];
+  iconosBotton.innerHTML = iconos[randomIcons];
+
+  if (randomIcons == 0 || randomIcons == 1) {
+    numeros.style.color = "red";
+    iconosTop.style.color = "red";
+    iconosBotton.style.color = "red";
+  } else {
+    numeros.style.color = "black";
+    iconosTop.style.color = "black";
+    iconosBotton.style.color = "black";
+  }
+}
+
+seleccionarCard();
